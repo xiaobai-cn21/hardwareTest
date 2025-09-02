7285dd5f16c8e46de9bf40b438cc7695756bc4d6
xiaobaicn-21 <josue2025cn@outlook.com>
2025-09-02 09:38:20 +0800

    new teest

--- test.cpp
+++ test.cpp
@@ -68,27 +68,27 @@ int main() {
     int equipment_number;
     std::cin >> equipment_number;
     if (equipment_number == 1) {
-        std::uniform_real_distribution<> temp(40.0, 80.0);  
-        std::uniform_real_distribution<> vibr(0.5, 2.0);   
-        std::uniform_real_distribution<> elect(150.0, 250.0);
-        std::uniform_real_distribution<> electPress(2.5, 4.0);
-        std::uniform_real_distribution<> vel(15000, 3000);
-        std::uniform_real_distribution<> temp2(35, 60);
+        std::uniform_real_distribution<> temp1(40.0, 80.0);  
+        std::uniform_real_distribution<> vibr1(0.5, 2.0);   
+        std::uniform_real_distribution<> elect1(150.0, 250.0);
+        std::uniform_real_distribution<> electPress1(2.5, 4.0);
+        std::uniform_real_distribution<> vel1(15000, 3000);
+        std::uniform_real_distribution<> temp21(35, 60);
     }
     else if(equipment_number == 2) {
-        std::uniform_real_distribution<> temp(40.0, 80.0);  
-        std::uniform_real_distribution<> vibr(0.5, 2.0);   
-        std::uniform_real_distribution<> elect(150.0, 250.0);
-        std::uniform_real_distribution<> electPress(2.5, 4.0);
-        std::uniform_real_distribution<> vel(15000, 30000);
-        std::uniform_real_distribution<> temp2(35, 60);
+        std::uniform_real_distribution<> temp1(40.0, 80.0);  
+        std::uniform_real_distribution<> vibr1(0.5, 2.0);   
+        std::uniform_real_distribution<> elect1(150.0, 250.0);
+        std::uniform_real_distribution<> electPress1(2.5, 4.0);
+        std::uniform_real_distribution<> vel1(15000, 3000);
+        std::uniform_real_distribution<> temp21(35, 60);
     } else {
-        std::uniform_real_distribution<> temp(40.0, 80.0);  
-        std::uniform_real_distribution<> vibr(0.5, 2.0);   
-        std::uniform_real_distribution<> elect(150.0, 250.0);
-        std::uniform_real_distribution<> electPress(2.5, 4.0);
-        std::uniform_real_distribution<> vel(15000, 3000);
-        std::uniform_real_distribution<> temp2(35, 60);
+        std::uniform_real_distribution<> temp1(40.0, 80.0);  
+        std::uniform_real_distribution<> vibr1(0.5, 2.0);   
+        std::uniform_real_distribution<> elect1(150.0, 250.0);
+        std::uniform_real_distribution<> electPress1(2.5, 4.0);
+        std::uniform_real_distribution<> vel1(15000, 3000);
+        std::uniform_real_distribution<> temp21(35, 60);
     }
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -98,25 +98,17 @@ int main() {
     std::cout << "Simulating sensor... (Press Ctrl+C to stop)\n";
     while (true) {
         // Generar datos ficticios
-        double temp = temp(gen);
-        double vibr = vibr(gen);
-        double elect = elect(gen);
-        double electPress = electPress(gen);
-        double vel = vel(gen);
-        double temp2 = temp2(gen);
+        double temp = temp1(gen);
+        double vibr = vibr1(gen);
+        double elect = elect1(gen);
+        double electPress = electPress1(gen);
+        double vel = vel1(gen);
+        double temp2 = temp21(gen);
 
         // Formatear datos (ej: "Temp=25.3,Press=1.05\n")
         // Usamos printf para controlar decimales (mejor que std::to_string)
         char dataBuffer[500];
-        snprintf(dataBuffer, sizeof(dataBuffer), 
-        "电机绕组温度=%.1f,
-        电机机壳振动=%.2f,
-        电机启动电流=%.2f
-        润滑系统压力=%.2f,
-        电机转速=%.2f,
-        润滑油温度=%.2f", 
-        temp, vibr, elect, electPress, vel, temp2 
-    );
+        snprintf(dataBuffer, sizeof(dataBuffer), "电机绕组温度=%.1f,电机机壳振动=%.2f,电机启动电流=%.2f润滑系统压力=%.2f,电机转速=%.2f,润滑油温度=%.2f", temp, vibr, elect, electPress, vel, temp2 );
         std::string data = dataBuffer;
 
         // Enviar datos a /dev/pts/1
