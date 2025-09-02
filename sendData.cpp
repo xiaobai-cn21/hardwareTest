612c345abd9c39e7a0f697effa639ed9fe293432
xiaobaicn-21 <josue2025cn@outlook.com>
2025-09-02 10:51:32 +0800

    add sendTest

---
+++ sendData.cpp
@@ -0,0 +1,58 @@
+// client.cpp
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    // 1. Create socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        std::cerr << "Socket creation failed\n";
+        return 1;
+    }
+
+    // 2. Server address setup
+    sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(9877); // Change to your server port if needed
+    if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0) { // Change IP if needed
+        std::cerr << "Invalid address\n";
+        return 1;
+    }
+
+    // 3. Connect to server
+    if (connect(sockfd, (sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "Connection failed\n";
+        return 1;
+    }
+    std::cout << "Connected to server!\n";
+
+    // 4. Send a string to server
+    std::string msg = "Hello, server!";
+    ssize_t sent = send(sockfd, msg.c_str(), msg.size(), 0);
+    if (sent < 0) {
+        std::cerr << "Send failed\n";
+        close(sockfd);
+        return 1;
+    }
+    std::cout << "Sent message to server.\n";
+
+    // 5. Try to receive response from server
+    char buf[1024] = {0};
+    ssize_t received = recv(sockfd, buf, sizeof(buf)-1, 0);
+    if (received > 0) {
+        std::cout << "Server response: " << buf << std::endl;
+    } else if (received == 0) {
+        std::cout << "Server closed connection.\n";
+    } else {
+        std::cout << "Receive failed.\n";
+    }
+
+    // 6. Close socket
+    close(sockfd);
+    return 0;
+}
